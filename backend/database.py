a203922c95a03a6ef0981a64241de5318149464a
dev1065 <devsharma1065@gmail.com>
2026-09-12 19:37:59 +0530

    made the inital models.py and the project table

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,7 @@
 import os
 
 from dotenv import load_dotenv
+from models import Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -17,6 +18,6 @@ SessionLocal = sessionmaker(
     autocommit=False
 )
 
-
+Base.metadata.create_all(bind=engine)
 with SessionLocal() as db:
     print("Successfull")
---
+++ backend/models.py
@@ -0,0 +1,24 @@
+from datetime import datetime, timezone
+
+from sqlalchemy import DateTime, String, Text
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Project(Base):
+    __tablename__ = "projects"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[String] = mapped_column(String(100))
+    description: Mapped[str | None] = mapped_column(Text, nullable=True)
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        onupdate=lambda: datetime.now(timezone.utc)
+    )
