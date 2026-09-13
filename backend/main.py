64f52df959c4b3cb688e031ce26d0e8a8d135264
dev1065 <devsharma1065@gmail.com>
2026-09-13 09:50:26 +0530

    added the post and get apis

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from database import get_db
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException
 from models import Project
 from schemas import ProjectCreate, ProjectResponse
 from sqlalchemy.orm import Session
@@ -32,3 +32,18 @@ def create_project(
 
     return project
 
+
+@app.get("/projects", response_model=list[ProjectResponse])
+def get_projects(db: Session = Depends(get_db)):  # noqa: B008
+    projects = db.query(Project).all()
+
+    return projects
+
+
+@app.get("/projects/{project_id}", response_model=ProjectResponse)
+def get_project(project_id: int, db: Session = Depends(get_db)):  # noqa: B008
+    project = db.query(Project).filter(Project.id == project_id).first()
+
+    if project is None:
+        raise HTTPException(status_code=404, detail="Project not found")
+    return project
